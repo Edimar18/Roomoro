09685a74e389bb46dcfaefd9b8fa6b6989e2985a
Edimar18 <mosquida.edimar.18@gmail.com>
2025-10-20 19:40:24 +0800

    added a map and a search bar on top

--- lib/screens/map_view.dart
+++ lib/screens/map_view.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
 
 class MapScreen extends StatelessWidget {
   const MapScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Map Screen Content'),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Search by area, landmark, or street",
+              prefixIcon: Icon(Icons.search, color: Colors.grey)
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.filter_list, color: Colors.blueAccent),
+            onPressed: () {
+              print("Pressed action button");
+            }
+          )
+
+        ],
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(8.4632, 124.6288),
+          initialZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.jegr.inc'
+          )
+        ])
     );
+
   }
 }
